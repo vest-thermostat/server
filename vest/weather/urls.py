030bafe6bda4b48929229528376b654b0d684e08
Perale Thomas <thomas.perale@openmailbox.org>
2016-11-20 15:43:41 +0100

    Changed path

--- vest/weather/urls.py
+++ vest/weather/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from weather import views
 
 urlpatterns = [
-    url(r'^weathers/$', views.weather_list),
-    url(r'^weathers/(?P<pk>[0-9]+)/$', views.weather_detail),
+    url(r'^api/$', views.weather_list),
+    url(r'^api/(?P<pk>[0-9]+)/$', views.weather_detail),
 ]
