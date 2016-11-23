89b4ba04bbcbf3ac3f31151f580a253475b85ff0
Corentin6 <ccandeur@ulb.ac.be>
2016-11-23 14:47:58 +0100

    .ve?

---
+++ .ve/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/corentin/Documents/Ulb/2016_2017/ProjetBA3/server/.ve/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
