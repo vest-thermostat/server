312c1e7c40ccd58dba279c0667f19386def75fc3
Perale Thomas <thomas.perale@openmailbox.org>
2016-12-07 20:05:12 +0100

    User profile view

--- vest/users/views.py
+++ vest/users/views.py
@@ -1,4 +1,3 @@
-from django.core.urlresolvers import reverse
 from django.views.generic import DetailView, UpdateView, CreateView
 from django.contrib.auth import views as auth_views
 from django.contrib.auth import login, logout, authenticate
@@ -18,30 +17,19 @@ class Registration(CreateView):
         login(self.request, new_user)
         return valid
 
-    # class Meta:
-    #     model = VestUser
-
-
-# class Login(auth_views.login):
-#     template_name = 'register.html'
-#     success_url = '/'
-
 
 class UserProfileView(DetailView):
-    pass
+    model = VestUser
+    template_name = "profile.html"
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
 
 class UserEditView(UpdateView):
     model = VestUser
     template_name = "edit.html"
     form_class = VestUserForm
-
-    def get_absolute_url(self):
-        return reverse('profile')
-
-    # def form_valid(self, form):
-    #     valid = super(Registration, self).form_valid(form)
-    #     print("IS VALID ???? : " + str(valid))
-    #     return valid
 
     def get_object(self, queryset=None):
         return self.request.user
