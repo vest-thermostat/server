504559dbabdba4bb59b907abedeef14dde454b53
Perale Thomas <thomas.perale@openmailbox.org>
2017-02-19 10:34:26 +0100

    Location REST API compatibility with GeoDjango

--- vest/location/serializers.py
+++ vest/location/serializers.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
 from location.models import UserLocation
+import rest_framework_gis.serializers as serializers
 
-class LocationSerializer(serializers.ModelSerializer):
+class LocationSerializer(serializers.GeoFeatureModelSerializer):
     """
     """
     class Meta:
         model = UserLocation
+        geo_field = "position"
         fields = (
             "created",
-            "latitude",
-            "longitude",
+            "owner",
+        )
+        read_only_fields = (
+            "created",
+            "owner",
         )
--- vest/location/views.py
+++ vest/location/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.exceptions import ValidationError
 
 from location.models import UserLocation
 from location.serializers import LocationSerializer
@@ -16,8 +17,13 @@ class LocationList(viewsets.ModelViewSet):
             IsOwner,
     )
 
+    def get_queryset(self, request):
+        queryset = UserLocation.objects.filter(owner=request.user)
+        serializer = LocationSerializer(queryset, many=True)
+        return Response(serializer.data)
+
     def perform_create(self, serializer):
-        if self.request.user.balance < serializer.price:
-            raise serializers.ValidationError("Pas assez d'argent dans la balance")
-        else:
-            serializer.save(owner=self.request.user)
+        if not serializer.is_valid():
+            raise ValidationError()
+
+        serializer.save(owner=self.request.user)
