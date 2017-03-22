b8c06d19cf3468c258a90d4f9b457b103491faa6
Perale Thomas <thomas.perale@openmailbox.org>
2017-03-22 10:33:42 +0100

    Only show location marker (route don't seem to work).

--- vest/assets/js/location_map/LocationMap.jsx
+++ vest/assets/js/location_map/LocationMap.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { OrderedSet } from 'immutable';
 import { NotificationStack } from 'react-notification';
-import { Map, Marker, Popup, TileLayer, GeoJSON } from 'react-leaflet';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
 import cookie from 'react-cookie';
 import axios from 'axios';
@@ -9,18 +9,25 @@ import RoutingMachine from './RoutingMap.jsx';
 
 import './LocationMap.scss';
 
+const MAX_SIZE = 20;
+
 export default class LocationMap extends React.Component {
     constructor (props) {
         super(props);
 
         let c = [50.81438, 4.38223];
 
+        let locs = this.props.locations.features;
+        if (locs && locs.length > MAX_SIZE) {
+            locs = locs.slice(0, MAX_SIZE);
+        }
+
         this.state = {
             current: c,
             notifications: OrderedSet(),
             count: 0,
             nests: [],
-            locations: props.locations.features,
+            locations: locs
         };
 
         this.addLocation = this.addLocation.bind(this);
@@ -34,7 +41,7 @@ export default class LocationMap extends React.Component {
                 let newLocations = this.state.locations
                 newLocations.splice(0, 0, json);
                 
-                if (newLocations.length > 100) {
+                if (newLocations.length > MAX_SIZE) {
                     newLocations.splice(newLocations.length - 1, 1);
                 }
                 this.setState({ locations: newLocations });
@@ -85,7 +92,7 @@ export default class LocationMap extends React.Component {
         //     console.log(JSON.stringify(json))
         //     if (json.type && json.geometry) {
         //         let newLocations = this.state.locations.concat([json]);
-        //         if (newLocations.length > 100) {
+        //         if (newLocations.length > MAX_SIZE) {
         //             newLocations.splice(0, 1);
         //         }
         //         this.setState({ locations: newLocations });
@@ -107,7 +114,17 @@ export default class LocationMap extends React.Component {
         };
     }
 
+    renderMarkers () {
+        return this.state.locations.map(x => {
+            const coord = x.geometry.coordinates;
+            return (
+                <Marker position={[coord[1], coord[0]]}/>
+            );
+        })
+    }
+
     render () {
+        // <RoutingMachine locations={this.state.locations}/>
         return (
             <div>
                 <Map center={this.state.current} zoom={13} onClick={this.addLocation} ref='map'>
@@ -115,7 +132,7 @@ export default class LocationMap extends React.Component {
                         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     />
-                    <RoutingMachine locations={this.state.locations}/>
+                    {this.renderMarkers ()}
                 </Map>
                 <NotificationStack
                     notifications={this.state.notifications.toArray()}
